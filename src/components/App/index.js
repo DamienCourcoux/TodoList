4393af3226c2c3b8592d15e576dc0e147665b387
DamienCourcoux <damsmailspro@gmail.com>
2021-09-20 18:38:34 +0200

    class + state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,17 +3,31 @@ import './app.scss';
 import Header from 'src/components/Header';
 import Todo from 'src/components/Todo';
 import tasksData from 'src/data/tasks';
+import React from 'react';
 
-// == Composant
-const App = () => (
-  <div className="app">
-    <Header />
-    <Todo
-      numberOfTodo={2}
-      tasks={tasksData}
-    />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numberOfTodo: 2,
+      tasks: tasksData,
+    };
+  }
+
+  render() {
+    const { numberOfTodo, tasks } = this.state;
+    return (
+      <div className="app">
+        <Header />
+        <Todo
+          numberOfTodo={numberOfTodo}
+          tasks={tasks}
+        />
+      </div>
+    );
+  }
+}
 
 // == Export
 export default App;
